32c46033bbf40cbd3b79819c7974b69cbbb53c50
PSSEA\bkielpinski <bryan.kielpinski52@gmail.com>
2018-10-01 07:45:02 -0600

    adding jagged array to array stuff file

--- array_stuff.cs
+++ array_stuff.cs
@@ -27,6 +27,18 @@ namespace ArrayApplication{
                 }
             }
             Console.ReadKey();
+
+            /* working with jagged arrays - A Jagged array is an array of arrays */
+            int[][] a = new int[][]{new int[]{0,0},new int[]{1,2}, new int[]{2,4},new int[]{ 3, 6 }, new int[]{ 4, 8 } };
+            int i, j;
+         
+            /* output each array element's value */
+            for (i = 0; i < 5; i++) {
+                for (j = 0; j < 2; j++) {
+                Console.WriteLine("a[{0}][{1}] = {2}", i, j, a[i][j]);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
